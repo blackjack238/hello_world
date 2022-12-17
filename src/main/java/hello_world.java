511c2c9b230c74981bddad376ef83f9514fa1997
blackjack238 <118196030+blackjack238@users.noreply.github.com>
2022-12-18 01:31:54 +0200

    fasd

--- src/main/java/hello_world.java
+++ src/main/java/hello_world.java
@@ -1,7 +1,53 @@
 package main.java;
 
-public class hello_world {public static void main(String[] args){
-    System.out.println("hello world");
+public class hello_world {public static void main(String[] args) {
+ /*int a = 1;
+ int f = 3;
+ System.out.println("a="+ a);
+ System.out.println("f=" +f);
+  int c = 1-10;
+  int d= 3+10;
+  System.out.println("c="+c);
+  System.out.println("d="+d);
+double ad = 15.0/10.0;
+int ag = (int) ad;
+
+  System.out.println(ad);
+  System.out.println(ag);
+  int k = 10 % 5;
+  System.out.println(k);*/
+ /* int x =0;
+  x= x+1;
+  x=x+4;
+  System.out.println(x);
+  double y=0;
+  y+=1.32/2;
+  y*=2.3452+0.234;
+  System.out.println(y);
+double ya=1;
+
+ya+=1*1934;
+  ya%=42.0;
+  System.out.println(ya);
+*/
+  /*int a = 2111532;
+int b=21114143;
+
+  System.out.println(a);
+  System.out.println(a<b);
+  System.out.println(a!=b);*/
+ /* boolean a= true;
+  boolean b=false;
+  System.out.println(!a);
+  System.out.println(!b);
+  System.out.println(a&&b);
+  System.out.println(a||b);
+  b= true;
+  System.out.println(a^b);*/
+//boolean result = getTrue()|getFalse();
+  long  longerThanLife = 2147483647;
+  char codeName70 ='70';
+  System.out.println(codeName70);
 }
 }
 
